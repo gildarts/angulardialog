389d3e1e03fdcfe1eadad36d5872529bf1912e4c
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-03-04 18:09:39 +0800

    還原。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-// import { MatDialog } from '@angular/material/dialog';
-// import { AlertComponent } from './alert/alert.component';
-// import { Overlay } from '@angular/cdk/overlay';
-// import { ComponentPortal } from '@angular/cdk/portal';
-// import { CustomDialogComponent } from './custom-dialog/custom-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertComponent } from './alert/alert.component';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { CustomDialogComponent } from './custom-dialog/custom-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
   title = 'ng9';
 
   constructor(
-    // private dialog: MatDialog,
-    // private overlay: Overlay
+    private dialog: MatDialog,
+    private overlay: Overlay
   ) {
 
   }
@@ -24,29 +24,29 @@ export class AppComponent implements OnInit {
   }
 
   alert() {
-    // this.dialog.open(AlertComponent, {
-    //   data: 'zoe'
-    // });
+    this.dialog.open(AlertComponent, {
+      data: 'zoe'
+    });
   }
 
   showOverlay() {
-    // const ol = this.overlay.create({
-    //   positionStrategy: this.overlay
-    //     .position()
-    //     .global()
-    //     .centerHorizontally()
-    //     .centerVertically(),
-    //   scrollStrategy: this.overlay
-    //     .scrollStrategies
-    //     .reposition(),
-    //   hasBackdrop: true,
-    // });
+    const ol = this.overlay.create({
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      scrollStrategy: this.overlay
+        .scrollStrategies
+        .reposition(),
+      hasBackdrop: true,
+    });
 
-    // ol.attach(new ComponentPortal(CustomDialogComponent));
+    ol.attach(new ComponentPortal(CustomDialogComponent));
 
-    // ol.backdropClick().subscribe(v => {
-    //   console.log(v);
-    //   ol.detach();
-    // })
+    ol.backdropClick().subscribe(v => {
+      console.log(v);
+      ol.detach();
+    })
   }
 }
